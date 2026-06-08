dc71bfd6158f1c79a5dcdc882ba6b25ea3517eeb
Mani3126 <naveen.mass6565@gmail.com>
2026-06-08 22:26:14 +0530

    updated to version 5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from DevOps Pipeline - Version 4! 🚀";
+        return "Hello from DevOps Pipeline - Version 5! 🚀";
     }
 }
